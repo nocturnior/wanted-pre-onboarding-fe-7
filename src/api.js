52edeb058632378ecc8dab043fb0991f131cd167
nocturnior <ssuperr_@naver.com>
2022-10-17 04:43:26 +0900

    :alien: [Feature] : api 수정중

--- src/api.js
+++ src/api.js
@@ -10,7 +10,7 @@ export const authApi = axios.create({
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+    Authorization: `Bearer ${localStorage.setItem('access_token')}`,
   },
   withCredentials: true,
 });
@@ -18,12 +18,10 @@ export const authApi = axios.create({
 // WHAT 비동기 실행
 authApi.interceptors.request.use(
   config => {
-    config.headers['Authorization'] = localStorage.getItem('token');
+    config.headers['Authorization'] = localStorage.getItem('access_token');
     return config;
   },
   err => {
     return Promise.reject(err);
   }
 );
-
-
--- src/apis/auth.js
+++ src/apis/auth.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
-import { authApi, setToken } from '../api';
+import { authApi } from '../api';
 
 export const userApis = {
   signin: async data => {
     const response = await authApi.post('/auth/signin', data);
-    if (response.status === 200) {
-      const token = { access_token: response.headers.authorization, refresh_token: response.headers['refresh-token'] };
-      setToken(token);
-    }
+    // if (response.status === 200) {
+    //   const token = { access_token: response.headers.authorization, refresh_token: response.headers['refresh-token'] };
+    //   setToken(token);
+    // }
     return response;
   },
   signup: async data => {
